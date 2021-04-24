c0ddaeb32786c7e8b845473e20b67e4c8d176bc1
Anar Kafkas <anarkafkas@gmail.com>
2021-04-24 21:04:37 +0300

    Integrate Eslint with Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,22 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  parser: "@typescript-eslint/parser",
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint"],
-  rules: {},
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    'no-constant-condition': 0,
+  },
 };
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,3 @@
 export function sayHello() {
-  return "Hello, World!";
+  return 'Hello, World!';
 }
