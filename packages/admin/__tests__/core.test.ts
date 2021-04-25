c7ad1fda5bf0c18c229739847e416d76a75face1
Anar Kafkas <anarkafkas@gmail.com>
2021-04-25 19:27:38 +0300

    Fix overload issue

--- packages/admin/__tests__/core.test.ts
+++ packages/admin/__tests__/core.test.ts
@@ -1,6 +1,23 @@
 import { firestore } from 'firebase-admin';
 import { CollectionTraverser, CollectionMigrator } from '../src';
 
+function doStuff(cb: (nowTimestamp: number) => void): void;
+
+function doStuff(obj: Record<string, number>, condition: string): void;
+
+function doStuff(
+  param1: ((nowTimestamp: number) => void) | Record<string, number>,
+  condition?: string
+) {
+  // ...
+}
+
+doStuff({}, '');
+
+doStuff((now) => {
+  //
+});
+
 const traverser = new CollectionTraverser(firestore().collection('users'), {
   batchSize: 500,
   sleepTimeBetweenBatches: 1000,
@@ -9,12 +26,37 @@ const traverser = new CollectionTraverser(firestore().collection('users'), {
 const migrator = new CollectionMigrator(firestore().collection('users'));
 
 (async () => {
-  const { updatedDocCount } = await migrator.update(
+  // S1
+
+  await migrator.update((snap) => {
+    console.log(snap.data());
+    return snap.exists;
+  });
+
+  await migrator.update(
+    (snap) => {
+      console.log(snap.data());
+      return snap.exists;
+    },
+    (snap) => snap.exists
+  );
+
+  // S2
+
+  await migrator.update({
+    test: 'abc',
+  });
+
+  await migrator.update(
     {
       isAdmin: false,
     },
-    (snapshot) => snapshot.data().isAdmin === undefined
+    (snap) => snap.data().isAdmin
   );
 
-  console.log(`Updated ${updatedDocCount} documents!`);
+  // S3
+
+  await migrator.update('some_field', 123, (snap) => snap.data().isAdmin);
+
+  await migrator.update('some_field', 123);
 })();
--- packages/admin/src/CollectionMigrator.ts
+++ packages/admin/src/CollectionMigrator.ts
@@ -15,6 +15,19 @@ export class CollectionMigrator<T> extends CollectionTraverser<T> {
    * Updates all documents in this collection with the provided update data. Uses batch writes so
    * the entire batch will fail if a single update isn't successful. This method uses the `.traverse()`
    * method internally to traverse the entire collection.
+   * @param getUpdateData - A function that returns an object with which to update each document (i.e. the `updateData` object).
+   * @param predicate - Optional. A function that returns a boolean indicating whether to update the current document. Takes the `QueryDocumentSnapshot` corresponding to the document as its first argument. If this is not provided, all documents will be updated.
+   * @returns The number of batches and documents updated.
+   */
+  public update(
+    getUpdateData: (snapshot: firestore.QueryDocumentSnapshot<T>) => firestore.UpdateData,
+    predicate?: UpdatePredicate<T>
+  ): Promise<UpdateResult>;
+
+  /**
+   * Updates all documents in this collection with the provided update data. Uses batch writes so
+   * the entire batch will fail if a single update isn't successful. This method uses the `.traverse()`
+   * method internally to traverse the entire collection.
    * @param updateData - The data with which to update each document. Must be a non-empty object.
    * @param predicate - Optional. A function that returns a boolean indicating whether to update the current document. Takes the `QueryDocumentSnapshot` corresponding to the document as its first argument. If this is not provided, all documents will be updated.
    * @returns The number of batches and documents updated.
@@ -39,67 +52,51 @@ export class CollectionMigrator<T> extends CollectionTraverser<T> {
     predicate?: UpdatePredicate<T>
   ): Promise<UpdateResult>;
 
-  /**
-   * Updates all documents in this collection with the provided update data. Uses batch writes so
-   * the entire batch will fail if a single update isn't successful. This method uses the `.traverse()`
-   * method internally to traverse the entire collection.
-   * @param getUpdateData - A function that returns an object with which to update each document (i.e. the `updateData` object).
-   * @param predicate - Optional. A function that returns a boolean indicating whether to update the current document. Takes the `QueryDocumentSnapshot` corresponding to the document as its first argument. If this is not provided, all documents will be updated.
-   * @returns The number of batches and documents updated.
-   */
-  public update(
-    getUpdateData: (snapshot: firestore.QueryDocumentSnapshot<T>) => firestore.UpdateData,
-    predicate?: UpdatePredicate<T>
-  ): Promise<UpdateResult>;
-
-  public async update(...args: unknown[]): Promise<UpdateResult> {
-    const handleUpdateBatchDoc = this.getUpdateHandlerForSignature(...args);
-
+  public async update(
+    arg1:
+      | firestore.UpdateData
+      | string
+      | firestore.FieldPath
+      | ((snapshot: firestore.QueryDocumentSnapshot<T>) => firestore.UpdateData),
+    arg2?: any,
+    arg3?: (snapshot: firestore.QueryDocumentSnapshot<T>) => boolean
+  ): Promise<UpdateResult> {
+    const argCount = [arg1, arg2, arg3].filter((a) => a !== undefined).length;
     const batch = this.collectionOrQuery.firestore.batch();
 
     const { batchCount, docCount: updatedDocCount } = await this.traverse(async (snapshots) => {
       snapshots.forEach((snapshot) => {
-        handleUpdateBatchDoc(batch, snapshot);
+        if (typeof arg1 === 'function') {
+          // Signature 1
+          const getUpdateData = arg1 as UpdateDataGetter<T>;
+          const predicate = arg2 as UpdatePredicate<T> | undefined;
+          const shouldUpdate = predicate?.(snapshot) ?? true;
+          if (shouldUpdate) {
+            batch.update(snapshot.ref, getUpdateData(snapshot));
+          }
+        } else if (argCount === 2 && typeof arg2 !== 'function') {
+          // Signature 3
+          const field = arg1 as string | firestore.FieldPath;
+          const value = arg2 as any;
+          const predicate = arg3 as UpdatePredicate<T> | undefined;
+          const shouldUpdate = predicate?.(snapshot) ?? true;
+          if (shouldUpdate) {
+            batch.update(snapshot.ref, field, value);
+          }
+        } else {
+          // Signature 2
+          const updateData = arg1 as firestore.UpdateData;
+          const predicate = arg2 as UpdatePredicate<T> | undefined;
+          const shouldUpdate = predicate?.(snapshot) ?? true;
+          if (shouldUpdate) {
+            batch.update(snapshot.ref, updateData);
+          }
+        }
       });
     });
 
     await batch.commit();
 
     return { batchCount, updatedDocCount };
-  }
-
-  private getUpdateHandlerForSignature(
-    ...args: unknown[]
-  ): (batch: firestore.WriteBatch, snapshot: firestore.QueryDocumentSnapshot<T>) => void {
-    const [arg1, arg2, arg3] = args;
-    const argCount = args.length;
-    return (batch, snapshot) => {
-      if (typeof arg1 === 'function') {
-        // Signature 3
-        const getUpdateData = arg1 as UpdateDataGetter<T>;
-        const predicate = arg2 as UpdatePredicate<T> | undefined;
-        const shouldUpdate = predicate?.(snapshot) ?? true;
-        if (shouldUpdate) {
-          batch.update(snapshot.ref, getUpdateData(snapshot));
-        }
-      } else if (argCount === 2 && typeof arg2 !== 'function') {
-        // Signature 2
-        const field = arg1 as string | firestore.FieldPath;
-        const value = arg2 as any;
-        const predicate = arg3 as UpdatePredicate<T> | undefined;
-        const shouldUpdate = predicate?.(snapshot) ?? true;
-        if (shouldUpdate) {
-          batch.update(snapshot.ref, field, value);
-        }
-      } else {
-        // Signature 1
-        const updateData = arg1 as firestore.UpdateData;
-        const predicate = arg2 as UpdatePredicate<T> | undefined;
-        const shouldUpdate = predicate?.(snapshot) ?? true;
-        if (shouldUpdate) {
-          batch.update(snapshot.ref, updateData);
-        }
-      }
-    };
   }
 }
