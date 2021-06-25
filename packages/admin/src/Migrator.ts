810a66d0dcf1d7a8fda8b198d5ae808121467172
Anar Kafkas <anarkafkas@gmail.com>
2021-06-25 19:37:42 +0300

    Fix jsdoc

--- packages/admin/src/Migrator.ts
+++ packages/admin/src/Migrator.ts
@@ -24,7 +24,7 @@ export abstract class Migrator<D extends firestore.DocumentData, C extends BaseT
   } = {};
 
   /**
-   * Registers a callback function that fires right before the current batch starts processing.
+   * Registers a callback function that fires right before a batch starts processing.
    * @param callback A synchronous callback that takes batch doc snapshots and the 0-based batch index as its arguments.
    */
   public onBeforeBatchStart(callback: BatchCallback<D>): void {
@@ -32,7 +32,7 @@ export abstract class Migrator<D extends firestore.DocumentData, C extends BaseT
   }
 
   /**
-   * Registers a callback function that fires after the current batch is processed.
+   * Registers a callback function that fires after a batch is processed.
    * @param callback A synchronous callback that takes batch doc snapshots and the 0-based batch index as its arguments.
    */
   public onAfterBatchComplete(callback: BatchCallback<D>): void {
