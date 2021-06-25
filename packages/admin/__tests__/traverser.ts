f34cd7817240edb891129a79428dbf21c5c13d12
Anar Kafkas <anarkafkas@gmail.com>
2021-06-25 17:17:34 +0300

    Commit before change

--- packages/admin/__tests__/traverser.ts
+++ packages/admin/__tests__/traverser.ts
@@ -1,5 +1,5 @@
 import { firestore } from 'firebase-admin';
-import { createBatchMigrator, createFastTraverser } from '../src';
+import { createBatchMigrator, createTraverser, createFastTraverser, FastTraverser } from '../src';
 
 type ProjectDoc = {
   isCompleted: boolean;
@@ -8,20 +8,31 @@ type ProjectDoc = {
 
 const projects = firestore().collection('projects') as firestore.CollectionReference<ProjectDoc>;
 
-const slowTraverser = createFastTraverser(projects);
 const fastTraverser = createFastTraverser(projects);
+const fastTraverserCI = new FastTraverser(projects);
+const slowTraverser = createTraverser(projects);
 
-slowTraverser.traverse((docs) => {
+fastTraverserCI.traverse(async (docs) => {
   //
 });
 
-fastTraverser.traverse((docs) => {
+const ssss = fastTraverserCI.withConfig({}).withConfig({});
+
+fastTraverser.traverse(async (docs) => {
   //
 });
+
+slowTraverser.traverse(async (docs) => {
+  //
+});
+
+const sss = slowTraverser.someWeirdStuff({});
+
+const slowTraversable = slowTraverser.traversable;
 
 fastTraverser.traversable; // Should be typed as a Query
 
-const defaultMigrator1 = createBatchMigrator(slowTraverser);
+const defaultMigrator1 = createBatchMigrator(fastTraverser);
 const defaultMigrator2 = createBatchMigrator(projects, { batchSize: 250 });
 
 const fastMigrator = createBatchMigrator(fastTraverser);
