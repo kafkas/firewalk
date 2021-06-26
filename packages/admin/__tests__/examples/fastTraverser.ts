2a5c830ccd1b05fa3f8799e5f450c1848b1d6404
Anar Kafkas <anarkafkas@gmail.com>
2021-06-26 20:39:59 +0300

    Add quickStart to examples

--- packages/admin/__tests__/examples/fastTraverser.ts
+++ packages/admin/__tests__/examples/fastTraverser.ts
@@ -4,9 +4,9 @@ import { createFastTraverser } from '../../src';
 const projectsColRef = firestore().collection('projects');
 const traverser = createFastTraverser(projectsColRef, {
   batchSize: 500,
+  // This means we are prepared to hold 500 * 20 = 10,000 docs in memory
   maxConcurrentBatchCount: 20,
 });
-// This means we should expect to hold 500 * 20 = 10,000 projects in memory
 const { docCount } = await traverser.traverse(async (_, batchIndex) => {
   console.log(`Gonna process batch ${batchIndex} now!`);
   // ...
---
+++ packages/admin/__tests__/examples/quickStart.ts
@@ -0,0 +1,18 @@
+import { firestore } from 'firebase-admin';
+import { createTraverser } from '../../src';
+
+const usersCollection = firestore().collection('users');
+const traverser = createTraverser(usersCollection);
+
+const { batchCount, docCount } = await traverser.traverse(async (batchDocs, batchIndex) => {
+  const batchSize = batchDocs.length;
+  await Promise.all(
+    batchDocs.map(async (snapshot) => {
+      const { email, firstName } = snapshot.data();
+      await sendEmail({ to: email, content: `Hello ${firstName}!` });
+    })
+  );
+  console.log(`Batch ${batchIndex} done! We emailed ${batchSize} users in this batch.`);
+});
+
+console.log(`Traversal done! We emailed ${docCount} users in ${batchCount} batches!`);
