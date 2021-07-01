cc103ad6929c8490aaba8f0be1e0ff3fcbe86dec
Anar Kafkas <anarkafkas@gmail.com>
2021-07-01 10:10:47 +0300

    Init jest

--- packages/admin/__tests__/singleProcessing.test.ts
+++ packages/admin/__tests__/singleProcessing.test.ts
@@ -1,28 +1,18 @@
-import { firestore } from 'firebase-admin';
-import { createTraverser, Traverser, TraversalConfig } from '../src';
+import { createTraverser } from '../src';
 import { app } from './app';
 
-async function testTraverser(
-  traverser: Traverser<firestore.DocumentData, TraversalConfig>
-): Promise<void> {
-  const processingCountMap: Record<string, number> = {};
-
+test('slow traverser processes each document exactly once w/o external interference', async () => {
+  const itemsColRef = app().admin.firestore().collection('items');
+  const processCountMap: Record<string, number> = {};
+  const traverser = createTraverser(itemsColRef, { batchSize: 5, maxDocCount: 25 });
   await traverser.traverse(async (snapshots) => {
     snapshots.forEach((snapshot) => {
       const itemId = snapshot.id;
-      processingCountMap[itemId] = (processingCountMap[itemId] ?? 0) + 1;
+      processCountMap[itemId] = (processCountMap[itemId] ?? 0) + 1;
     });
   });
-
-  // TODO: Assert that the value of each key in `processingCountMap` equals 1
-}
-
-async function main(): Promise<void> {
-  const itemsColRef = app().admin.firestore().collection('items');
-  const traverser = createTraverser(itemsColRef, { batchSize: 10, maxDocCount: 100 });
-
-  // Run a simple test without external interference
-  await testTraverser(traverser);
-}
-
-main();
+  const isEachProcessCountEqualTo1 = Object.keys(processCountMap).every(
+    (key) => processCountMap[key] === 1
+  );
+  expect(isEachProcessCountEqualTo1).toBe(true);
+});
