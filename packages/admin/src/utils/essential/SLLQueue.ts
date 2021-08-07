ed74921264dee7e8c06a1a5adcb6912a7c7f10e0
Anar Kafkas <anarkafkas@gmail.com>
2021-08-07 13:48:13 +0400

    Implement dequeueFirst() method on SLLQueue

--- packages/admin/src/utils/essential/SLLQueue.ts
+++ packages/admin/src/utils/essential/SLLQueue.ts
@@ -39,11 +39,31 @@ export class SLLQueue<E> {
     this.itemCount++;
   }
 
+  /**
+   * Dequeues all items into an array.
+   *
+   * @returns The array of dequeued items.
+   */
   public dequeueAll(): E[] {
-    const len = this.size;
-    const items = new Array<E>(len);
+    return this.dequeueFirst(this.size);
+  }
 
-    for (let i = 0; i < len; i++) {
+  /**
+   * Dequeues the first `itemCount` items into an array.
+   *
+   * @param itemCount - The number of items to dequeue.
+   * @returns The array of dequeued items.
+   */
+  public dequeueFirst(itemCount: number): E[] {
+    if (itemCount < 0 || itemCount > this.size || !Number.isInteger(itemCount)) {
+      throw new Error(
+        'The `itemCount` argument must be a non-negative integer less than the size of the queue.'
+      );
+    }
+
+    const items = new Array<E>(itemCount);
+
+    for (let i = 0; i < itemCount; i++) {
       items[i] = this.dequeue();
     }
 
