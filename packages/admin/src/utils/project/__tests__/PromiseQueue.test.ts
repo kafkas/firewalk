d2a4d0f1bc0b2e5f34449ef913659efdf4bf58bc
Anar Kafkas <anarkafkas@gmail.com>
2021-08-11 21:09:23 +0300

    Add a couple more PromiseQueue tests

--- packages/admin/src/utils/project/__tests__/PromiseQueue.test.ts
+++ packages/admin/src/utils/project/__tests__/PromiseQueue.test.ts
@@ -4,17 +4,38 @@ import { PromiseQueue } from '../PromiseQueue';
 describe('PromiseQueue', () => {
   const runTask = (): Promise<void> => sleep(0);
 
-  test('always shows correct size', async () => {
+  test('correctly processes all items', async () => {
     const q = new PromiseQueue<void>();
 
     expect(q.size).toBe(0);
 
     q.enqueue(runTask());
     q.enqueue(runTask());
+    q.enqueue(runTask());
+
+    expect(q.size).toBe(3);
+
+    await q.processAll();
+
+    expect(q.size).toBe(0);
+  });
+
+  test('correctly processes the first `n` items', async () => {
+    const q = new PromiseQueue<void>();
+
+    q.enqueue(runTask());
+    q.enqueue(runTask());
+    q.enqueue(runTask());
+
+    await q.processFirst(0);
+
+    expect(q.size).toBe(3);
+
+    await q.processFirst(1);
 
     expect(q.size).toBe(2);
 
-    await q.process();
+    await q.processFirst(2);
 
     expect(q.size).toBe(0);
   });
