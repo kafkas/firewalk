261102de6e069c95765d430859caaa261cdbb946
Anar Kafkas <anarkafkas@gmail.com>
2021-10-17 10:35:07 +0300

    Update deleteFields test

--- packages/admin/src/implementations/__tests__/Migrator/shared/deleteFields.ts
+++ packages/admin/src/implementations/__tests__/Migrator/shared/deleteFields.ts
@@ -14,14 +14,18 @@ export function testDeleteFields<C extends TraversalConfig>(
   migrationTester(colRef).test(
     'correctly deletes a single field from each doc',
     async (initialData) => {
-      await migrator.deleteFields(new firestore.FieldPath('map1', 'string1'), 'num2', 'timestamp1');
+      await migrator.deleteFields(
+        new firestore.FieldPath('map1', 'num1'),
+        new firestore.FieldPath('map1', 'string1'),
+        'timestamp1'
+      );
       const { docs } = await colRef.get();
       docs.forEach((snap) => {
         const data = snap.data();
         const expected = (() => {
           const copy = { ...cloneDeep(initialData) };
+          delete copy.map1.num1;
           delete copy.map1.string1;
-          delete copy.num2;
           delete copy.timestamp1;
           return copy;
         })();
