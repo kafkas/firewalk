71a74550b828ca9d09c406f8f9679365486dc4d4
Anar Kafkas <anarkafkas@gmail.com>
2022-02-05 15:11:40 +0300

    Docs for error classes

--- packages/admin/src/errors/ImplementationError.ts
+++ packages/admin/src/errors/ImplementationError.ts
@@ -1,3 +1,7 @@
+/**
+ * An error thrown when a piece of code is found to be incorrectly implemented. If the maintainers of the
+ * library have done everything right, this error should never be encountered.
+ */
 export class ImplementationError extends Error {
   constructor(message: string) {
     super(`Implementation Error: ${message}`);
--- packages/admin/src/errors/InvalidConfigError.ts
+++ packages/admin/src/errors/InvalidConfigError.ts
@@ -1,3 +1,6 @@
+/**
+ * An error thrown when an invalid configuration is provided.
+ */
 export class InvalidConfigError extends Error {
   constructor(message: string) {
     super(`Invalid config: ${message}`);
