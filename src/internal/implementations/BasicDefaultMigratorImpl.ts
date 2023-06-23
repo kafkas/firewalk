237c3d5ef7a4e90036c6d6925a22711043f5c402
Anar Kafkas <anarkafkas@gmail.com>
2023-06-23 16:28:26 +0300

    Format code

--- src/internal/implementations/BasicDefaultMigratorImpl.ts
+++ src/internal/implementations/BasicDefaultMigratorImpl.ts
@@ -131,7 +131,7 @@ export class BasicDefaultMigratorImpl<D> extends AbstractMigrator<D> implements 
 
       if (Array.isArray(data)) {
         // Signature 2
-        await doc.ref.update(...data as [string | firestore.FieldPath, any, ...any[]]);
+        await doc.ref.update(...(data as [string | firestore.FieldPath, any, ...any[]]));
       } else {
         // Signature 1
         if (precondition === undefined) {
