e693dcdc6a7914d43118c71a7c8d30012390f8b7
Luke Rogers <8382642+luke-rogers@users.noreply.github.com>
2023-12-05 14:46:52 +0000

    fix: tidy up

--- __tests__/app.ts
+++ __tests__/app.ts
@@ -11,9 +11,6 @@ class Application {
 let _app: Application | undefined;
 
 export function app(): Application {
-  process.env.FIRESTORE_EMULATOR_HOST = `127.0.0.1:8080`;
-  process.env.FIREBASE_STORAGE_EMULATOR_HOST = `127.0.0.1:9199`;
-  process.env.FIREBASE_EMULATOR_HUB = '127.0.0.1:4400';
   const firebaseApp = admin.initializeApp();
   return _app ?? (_app = new Application(firebaseApp));
 }
