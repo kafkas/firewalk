a3735c8b44d5f28a3f3e1d4999caf05474da3a8f
Anar Kafkas <anarkafkas@gmail.com>
2023-12-26 05:52:31 +0900

    Init generate-docs script

---
+++ scripts/generate-docs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Run from project root
+
+# Parse version
+complete_version=$(npm pkg get version | tr -d '"')
+
+# Extract the major version
+major_version=$(echo $complete_version | awk -F '.' '{print $1}')
+
+typedoc --out docs/v$major_version
