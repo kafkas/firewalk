637f6f000b0e819519b05f259c8769380112ff74
Anar Kafkas <anarkafkas@gmail.com>
2025-06-15 09:14:40 +0300

    Format

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,12 @@
 // babel.config.js
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: { node: 'current' },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
 };
