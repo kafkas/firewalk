beccd5927694d92bcd3d37bbe3e9dab87dad688b
Anar Kafkas <anarkafkas@gmail.com>
2025-06-15 09:04:31 +0300

    Fixes

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -7,4 +7,5 @@ complete_version=$(npm pkg get version | tr -d '"')
 # Extract the major version
 major_version=$(echo $complete_version | awk -F '.' '{print $1}')
 
+# Generate docs
 typedoc --out docs/v$major_version
